b3fabd39f7886b7d6fb5e8445188d2202819e845
Yongjin <yjjiangphysics@gmail>
2018-12-17 08:43:52 -0600

    update about page

---
+++ About me_files/Solved/logic.js
@@ -0,0 +1,64 @@
+// Create a map object
+var myMap = L.map("map", {
+  center: [37.09, -95.71],
+  zoom: 2
+});
+
+// Add a tile layer
+L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.streets",
+  accessToken: API_KEY
+}).addTo(myMap);
+
+// An array containing each city's name, location, and population
+var cities = [{
+  location: [40.7128, -74.0059],
+  name: "New York",
+  population: "8,550,405"
+},
+{
+  location: [41.8781, -87.6298],
+  name: "Chicago",
+  population: "2,720,546"
+},
+{
+  location: [29.7604, -95.3698],
+  name: "Houston",
+  population: "2,296,224"
+},
+{
+  location: [34.0522, -118.2437],
+  name: "Los Angeles",
+  population: "3,971,883"
+},
+        
+{
+  location: [23.697809, 120.960518],
+  name: "Taiwan",
+  population: "446,599"
+},   
+              
+{
+  location: [22.396427, 114.109497],
+  name: "HongKong",
+  population: "446,599"
+},            
+  
+{  
+  location: [36.204823, 138.252930],
+  name: "Japan",
+  population: "446,599"
+},
+    
+              
+];
+
+// Loop through the cities array and create one marker for each city, bind a popup containing its name and population add it to the map
+for (var i = 0; i < cities.length; i++) {
+  var city = cities[i];
+  L.marker(city.location)
+    .bindPopup("<h1>" + city.name + "</h1> <hr> <h3>Population " + city.population + "</h3>")
+    .addTo(myMap);
+}
